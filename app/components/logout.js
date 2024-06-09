1355a3d3c81c597e801ce2e3e484d28f41df46ad
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-06-09 20:37:24 +0300

    edit style of logout waiting spinner

--- app/components/logout.js
+++ app/components/logout.js
@@ -13,15 +13,15 @@ export default function Logout() {
     return (
         <>
         {
-        isLoading ? <Spinner
+        isLoading ? <span className="px-9"><Spinner
         size="3"
-        className="text-purple-950 animate-spin"
-      /> :
-        <li className="flex gap-2 py-1 px-2 text-white bg-sky-600 hover:bg-white hover:text-sky-600 border rounded-lg md:border-none hover:cursor-pointer">
+        className="text-sky-900"
+      /></span> :
+        <li  onClick={async () => await handleLogout()} className="flex gap-2 py-1 px-2 text-white bg-sky-600 hover:bg-white hover:text-sky-600 border rounded-lg md:border-none hover:cursor-pointer">
             <PowerIcon className="w-6 h-6" />
-            <button onClick={() => handleLogout()} className="text-center ">
+            <span className="flex items-center justify-center text-center">
                 Logout
-            </button>
+            </span>
         </li>
         }
         </>
